d13eb30609525962199ced7ff5841c0157897120
Piotr Jura <jura.piotrek@gmail.com>
2021-08-17 19:41:52 +0200

    0144-component-custom-events

--- app.js
+++ app.js
@@ -15,21 +15,41 @@ app.component('todo-list-item', {
       //   return "Okay" === value;
       // }
     },
-    id: {
-      required: false,
-      type: Number,
-      validator(value) {
-        return value >= 1 && value <= 100;
-      },
-      // default() {
-      //   return [];
-      // }
-    }
+    // id: {
+    //   required: false,
+    //   type: Number,
+    //   validator(value) {
+    //     return value >= 1 && value <= 100;
+    //   },
+    //   // default() {
+    //   //   return [];
+    //   // }
+    // }
   },
   template: `<div 
     class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4">
-      {{task}} {{id}}
+      {{task}}
     </div>`
+});
+
+app.component('add-task-input', {
+  emits: ['added'],
+  data() {
+    return {
+      task: ''
+    }
+  },
+  methods: {
+    add() {
+      this.$emit('added', this.task);
+      this.task = '';
+    }
+  },
+  template: `<input type="text" 
+    placeholder="Enter task and hit enter"
+    @keyup.enter="add"
+    v-model="task"
+    class="block w-full rounded-md shadow-sm text-lg p-4" />`
 });
 
 app.mount('#app');
