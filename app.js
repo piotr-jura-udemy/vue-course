3511c5bf1548c8d28a79088d23ff073955dab8e5
Piotr Jura <jura.piotrek@gmail.com>
2021-08-17 13:17:59 +0200

    0143-typing-props

--- app.js
+++ app.js
@@ -7,8 +7,29 @@ const app = Vue.createApp({
 });
 
 app.component('todo-list-item', {
-  props: ['task'],
-  template: `<div class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4">{{task}}</div>`
+  props: {
+    task: {
+      type: String,
+      required: true,
+      // validator(value) {
+      //   return "Okay" === value;
+      // }
+    },
+    id: {
+      required: false,
+      type: Number,
+      validator(value) {
+        return value >= 1 && value <= 100;
+      },
+      // default() {
+      //   return [];
+      // }
+    }
+  },
+  template: `<div 
+    class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4">
+      {{task}} {{id}}
+    </div>`
 });
 
 app.mount('#app');
