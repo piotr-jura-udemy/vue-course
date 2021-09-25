ac713ceac3f04fb12dd4caf97a475f3c365019eb
Piotr Jura <jura.piotrek@gmail.com>
2021-09-25 17:46:35 +0200

    0186-moving-to-sfc

---
+++ single-file-components/src/components/AddTaskInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+    type="text"
+    ref="input"
+    placeholder="Enter task and hit enter"
+    @keyup.enter="add"
+    v-model="task"
+    class="block w-full rounded-md shadow-sm text-lg p-4"
+  />
+</template>
+
+<script>
+export default {
+  emits: ["added"],
+  data() {
+    return {
+      task: "",
+    };
+  },
+  methods: {
+    add() {
+      this.$emit("added", this.task);
+      this.task = "";
+    },
+  },
+  mounted() {
+    this.$refs.input.focus();
+  },
+};
+</script>
---
+++ single-file-components/src/components/BaseCheckbox.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex items-center">
+    <input
+      type="checkbox"
+      class="h-4 w-4 text-indigo-600 border-gray-300 rounded mr-2"
+      :checked="modelValue"
+      @change="onChange"
+    />
+    <label><slot>Checkbox</slot></label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    onChange() {
+      this.$emit("update:modelValue", !this.modelValue);
+    },
+  },
+};
+</script>
