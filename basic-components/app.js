e4d0c42b97978911b3b7a69cfaa70fef0cfa0dd6
Piotr Jura <jura.piotrek@gmail.com>
2021-09-25 17:46:56 +0200

    0186-moving-to-sfc

--- basic-components/app.js
+++ basic-components/app.js
@@ -1,27 +1,9 @@
-// Things covered in code:
-// - Registering component (global)
-// - Refs (using ref to reference DOM)
-// - Emitting custom events
-// - Emitting value with an event
-// - Handling custom component events
-// - Props (passing data to component)
-// - Props typing
-// - Defining emitted events
-// - Component reusability (using mutliple checkboxes)
-// Needs explanation:
-// - How to reference component by name?
-// - Using v-model on custom components
-// - Slots
-// - Dynamic components
-// - DOM template caveats
-// - Element placement restrictions
-// - Case sensitivity?
-
 let nextTaskId = 100;
 
 const app = Vue.createApp({
   data() {
     return {
+      onlyPending: false,
       tasks: [{
         id: 1,
         description: 'Buy food for the dog',
@@ -45,8 +27,17 @@ const app = Vue.createApp({
         description: 'Go to the gym',
         priority: false,
         done: false
-      }],
-      onlyPending: false
+      }]
+    }
+  },
+  computed: {
+    displayedTasks() {
+      return [...this.tasks].sort(
+        (a, b) => Number(b.priority) - Number(a.priority)
+      )
+        .filter(
+          task => !this.onlyPending || !task.done
+        );
     }
   },
   methods: {
@@ -57,32 +48,49 @@ const app = Vue.createApp({
         done: false,
         priority: false
       });
-    },
-    taskToggled(task) {
-      console.log(`Toggling task ${task.id}`);
-      const foundTask = this.tasks
-        .find(t => t.id === task.id);
-
-      if (foundTask) {
-        foundTask.done = task.done;
-      }
-    },
-    taskPrioritized(task) {
-      console.log(`Prioritizing task ${task.id}`);
-      this.tasks.find(t => t.id === task.id)
-        .priority = task.priority;
-    }
-  },
-  computed: {
-    sortedTasks() {
-      return [...this.tasks].sort(
-        (a, b) => Number(b.priority) - Number(a.priority)
-      ).filter(task => !this.onlyPending || !task.done);
     }
   }
 });
 
-app.component('add-task', {
+app.component('todo-list-item', {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+      // validator(value) {
+      //   return "Okay" === value;
+      // }
+    },
+    done: Boolean,
+    priority: Boolean
+    // id: {
+    //   required: false,
+    //   type: Number,
+    //   validator(value) {
+    //     return value >= 1 && value <= 100;
+    //   },
+    //   // default() {
+    //   //   return [];
+    //   // }
+    // }
+  },
+  emits: ['update:done', 'update:priority'],
+  template: `<div 
+    class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4"
+    :class="{'opacity-25 line-through': task.done}">
+        <div>{{task.description}}</div>
+        <div class="py-4 bg-white">
+        <base-checkbox class="mb-2"
+          @update:model-value="$emit('update:done', $event)"
+          :model-value="done">Done</base-checkbox>
+        <base-checkbox
+          @update:model-value="$emit('update:priority', $event)"
+          :model-value="priority">Prioritized</base-checkbox>
+      </div>
+    </div>`
+});
+
+app.component('add-task-input', {
   emits: ['added'],
   data() {
     return {
@@ -93,41 +101,17 @@ app.component('add-task', {
     add() {
       this.$emit('added', this.task);
       this.task = '';
-    },
+    }
   },
   mounted() {
     this.$refs.input.focus();
   },
-  template: `
-    <input type="text" 
-      class="block w-full rounded-md shadow-sm text-lg p-4" 
-      placeholder="Enter task and hit enter" 
-      @keyup.enter="add" 
-      v-model="task"
-      ref="input" />`
-});
-
-app.component('todo-item', {
-  props: {
-    task: Object,
-    done: Boolean,
-    priority: Boolean,
-  },
-  emits: ['update:done', 'update:priority'],
-  template: `
-    <div 
-      class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm" 
-      :class="{'opacity-25 line-through': task.done}">
-    <div class="p-4 border-b border-gray-100">
-      {{task.description}}
-    </div>
-    <div class="p-4 bg-white">
-      <base-checkbox @update:model-value="$emit('update:done', $event)"
-        :model-value="done" label="Done" class="mb-1"></base-checkbox>
-      <base-checkbox @update:model-value="$emit('update:priority', $event)"
-        :model-value="priority" label="Prioritized"></base-checkbox>
-    </div>
-  </div>`
+  template: `<input type="text" 
+    ref="input"
+    placeholder="Enter task and hit enter"
+    @keyup.enter="add"
+    v-model="task"
+    class="block w-full rounded-md shadow-sm text-lg p-4" />`
 });
 
 app.component('base-checkbox', {
@@ -136,23 +120,22 @@ app.component('base-checkbox', {
       type: Boolean,
       default: false
     },
-    label: {
-      type: String
-    }
   },
   emits: ['update:modelValue'],
-  template: `
-    <div class="flex items-center">
-      <input type="checkbox" 
-        @change="onChange" :checked="modelValue"
-        class="h-4 w-4 text-indigo-600 border-gray-300 rounded mr-2"/>
-      <label>{{label}}</label>
-    </div>`,
   methods: {
     onChange() {
-      this.$emit('update:modelValue', !this.modelValue);
+      this.$emit(
+        'update:modelValue', !this.modelValue
+      );
     }
-  }
+  },
+  template: `<div class="flex items-center">
+    <input type="checkbox" 
+      class="h-4 w-4 text-indigo-600 border-gray-300 rounded mr-2"
+      :checked="modelValue"
+      @change="onChange"/>
+    <label><slot>Checkbox</slot></label>
+  </div>`
 });
 
 app.mount('#app');
