20fb2e4b64393014ad74757e1f4d09c35d45a875
Piotr Jura <jura.piotrek@gmail.com>
2021-10-09 14:25:41 +0200

    0187-0188-vue-cli-configuration

---
+++ single-file-components/src/Landing.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <div class="text-center">
+        <h3 class="text-3xl mb-2">Landing Page of Todo App!</h3>
+        <a href="/" class="underline">Go to the app!</a>
+      </div>
+    </div>
+  </div>
+</template>
