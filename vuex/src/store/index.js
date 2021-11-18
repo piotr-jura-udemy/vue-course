751eea22e5e9dec10ab427b2edd64e1b428d5acc
Piotr Jura <jura.piotrek@gmail.com>
2021-11-18 18:58:53 +0100

    VueX - Constants as mutation types

--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -1,5 +1,11 @@
 import { createStore } from "vuex";
 import { localStoragePlugin } from "./plugins/localStorage";
+import {
+  ADD_TASK,
+  UPDATE_TASK,
+  SET_ONLY_PENDING,
+  SET_ACTIVE_PROJECT,
+} from "./mutation-types";
 
 function getProjectById(state, id) {
   return state.projects.find((project) => project.id === id);
@@ -124,10 +130,10 @@ const store = createStore({
       }
     }
     */
-    addTask(state, payload) {
+    [ADD_TASK](state, payload) {
       getProjectById(state, payload.projectId)?.tasks.push(payload.task);
     },
-    updateTask(state, payload) {
+    [UPDATE_TASK](state, payload) {
       const project = getProjectById(state, payload.projectId);
       const taskIndex = project?.tasks?.findIndex(
         (task) => task.id === payload.task.id
@@ -137,10 +143,10 @@ const store = createStore({
         project.tasks[taskIndex] = payload.task;
       }
     },
-    setOnlyPending(state, payload) {
+    [SET_ONLY_PENDING](state, payload) {
       state.onlyPending = payload;
     },
-    setActiveProject(state, activeProjectId) {
+    [SET_ACTIVE_PROJECT](state, activeProjectId) {
       state.activeProjectId = activeProjectId;
     },
   },
---
+++ vuex/src/store/mutation-types.js
@@ -0,0 +1,4 @@
+export const ADD_TASK = "ADD_TASK";
+export const UPDATE_TASK = "UPDATE_TASK";
+export const SET_ONLY_PENDING = "SET_ONLY_PENDING";
+export const SET_ACTIVE_PROJECT = "SET_ACTIVE_PROJECT";
