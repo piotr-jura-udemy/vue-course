cc96947670a2121ea847e500ad35cd140dbb86eb
Piotr Jura <jura.piotrek@gmail.com>
2021-12-30 09:19:14 +0100

    0402-ref-and-reactive

--- composition-api/src/App.vue
+++ composition-api/src/App.vue
@@ -1,26 +1,21 @@
 <script>
-import { ref, computed } from "vue"
+import { ref, computed, reactive } from "vue"
 const START_FROM = 10
 export default {
-  // data() {
-  //   return {
-  //     counter: 0
-  //   }
-  // },
-  // computed: {
-  //   multiplied() {
-  //     return this.counter * 10;
-  //   }
-  // },
   setup() {
     const counter = ref(0)
     const multiplied = computed(
       () => counter.value * 10
     )
+    const xxx = reactive({
+      counter: 10
+    })
+    const ra = ref([1, 2, 3])
+    const replaceArray = () => ra.value = [4, 5, 6]
     const increase = () => counter.value++
     return {
       counter, multiplied, START_FROM,
-      increase
+      increase, xxx, replaceArray, ra
     }
   }
 }
@@ -29,4 +24,9 @@ export default {
 <template>
   <div>Hello! {{ counter }}</div>
   <button @click="increase">Increase</button>
+
+  <button @click="replaceArray">Replace</button>
+
+  <div>{{ xxx.counter }}</div>
+  <div>{{ ra }}</div>
 </template>
