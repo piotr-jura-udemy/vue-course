d80d363b8b6ee1c0a89a5107f946c42bdc86ea34
Piotr Jura <jura.piotrek@gmail.com>
2022-03-06 12:02:41 +0100

    0613-updating-documents-update-set-doc

--- todo-firebase/src/components/project/ProjectListItem.vue
+++ todo-firebase/src/components/project/ProjectListItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    @click="activateProject(project.id)"
+    @click="setActiveProjectId(project.id)"
     class="rounded-md py-2 px-2 text-gray-500 font-semibold flex justify-between cursor-pointer"
     :class="{ 'bg-gray-200': isActive }"
   >
@@ -11,17 +11,12 @@
 
 <script setup>
 import { computed, inject } from "vue";
-import { useStore } from "vuex";
-import { SET_ACTIVE_PROJECT } from "../../store/mutation-types";
+import { setActiveProjectId } from "./../../firebase/user"
 
 const props = defineProps({ project: Object });
-const store = useStore();
-// const activeProjectId = computed(() => store.state.project.activeProjectId);
 const activeProjectId = inject("activeProjectId")
 const isActive = computed(() => activeProjectId.value === props.project.id);
 const notDone = computed(
   () => props.project.taskCount - props.project.taskDoneCount
 )
-const activateProject = (projectId) =>
-  store.commit(`project/${SET_ACTIVE_PROJECT}`, projectId);
 </script>
--- todo-firebase/src/firebase/user.js
+++ todo-firebase/src/firebase/user.js
@@ -1,4 +1,4 @@
-import { onSnapshot, doc } from "firebase/firestore"
+import { onSnapshot, doc, updateDoc } from "firebase/firestore"
 import { ref, onUnmounted } from "vue"
 import { db } from "./firebase"
 
@@ -11,3 +11,12 @@ export const useUserProfile = () => {
   onUnmounted(unsub)
   return userProfile
 }
+
+export const setActiveProjectId =
+  async (activeProjectId) => updateDoc(
+    doc(db, "profiles", "me"),
+    {
+      activeProjectId
+    }
+  )
+
