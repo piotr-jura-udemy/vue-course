277719b09501e88cb793e8b7c4acdc7f844a9ee5
Piotr Jura <jura.piotrek@gmail.com>
2022-03-13 19:18:52 +0100

    0701-sign-in-sign-out-auth-state

--- todo-firebase/src/firebase/user.js
+++ todo-firebase/src/firebase/user.js
@@ -1,6 +1,38 @@
 import { onSnapshot, doc, updateDoc } from "firebase/firestore"
 import { ref, onUnmounted } from "vue"
 import { db } from "./firebase"
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth"
+
+export const user = ref(null)
+
+const auth = getAuth()
+onAuthStateChanged(auth, (data) => {
+  if (data) {
+    user.value = data
+  } else {
+    user.value = null
+  }
+
+  console.log(user.value)
+})
+
+export const login = async (email, password) => {
+  const result = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  )
+}
+
+export const logout = async () => {
+  await signOut(auth)
+}
+
+login(
+  'jura.piotrek@gmail.com',
+  'Jura13151719'
+)
+setTimeout(logout, 10000)
 
 export const useUserProfile = () => {
   const userProfile = ref({})
