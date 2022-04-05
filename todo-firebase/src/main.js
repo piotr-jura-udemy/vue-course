cf48be2aab4e44337fdbc5e0d4308852d52773ec
Piotr Jura <jura.piotrek@gmail.com>
2022-04-05 18:00:26 +0200

    0801-configuring-tailwind-css

--- todo-firebase/src/main.js
+++ todo-firebase/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import store from "./store"
 import router from "./router"
+import "./index.css"
 
 const app = createApp(App)
 app.config.unwrapInjectedRef = true
