26ca56868087ca27278f9e960299fc9cdad0af33
Piotr Jura <jura.piotrek@gmail.com>
2022-04-16 18:55:06 +0200

    0857-named-transitions

--- transitions-animations/transition/src/Fade.vue
+++ transitions-animations/transition/src/Fade.vue
@@ -5,24 +5,7 @@ const show = ref(true)
 
 <template>
   <button @click="show = !show">Toggle</button>
-  <Transition>
+  <Transition name="fade">
     <p v-if="show">Fade animation</p>
   </Transition>
 </template>
-
-<style scoped>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 2s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-
-.v-enter-to,
-.v-leave-from {
-  opacity: 0.5;
-}
-</style>
