d84354d2b1cdfcb397077f5e20193694a6b315b9
Piotr Jura <jura.piotrek@gmail.com>
2022-05-26 13:29:36 +0200

    1003-testing-api-overview

--- first-unit-test/test/basic.test.js
+++ first-unit-test/test/basic.test.js
@@ -3,3 +3,20 @@ import { test, expect } from 'vitest'
 test('2+2=4', () => {
   expect(2 + 2).toBe(4)
 })
+
+const iAmHere = null
+
+test('Testing the API', () => {
+  // expect(0.2 + 0.1).toBe(0.3)
+  expect(0.2 + 0.1).toBeCloseTo(0.3)
+  expect(iAmHere).toBeDefined()
+  expect('Hello').toBeTruthy()
+})
+
+class Example { }
+const example = new Example
+
+test('The object is right', () => {
+  expect(example).toBeDefined()
+  expect(example).toBeInstanceOf(Example)
+})
